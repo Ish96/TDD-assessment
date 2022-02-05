7ce8ea674f5119fca44e2f6ecba5ae7d12fb012a
Ishan Shah <imshah96@gmail.com>
2022-02-05 20:10:32 +0530

    Refactor Add method to handle negative numbers

--- src/main/java/com/ishan/calculate/SimpleCalculator.java
+++ src/main/java/com/ishan/calculate/SimpleCalculator.java
@@ -1,6 +1,8 @@
 package com.ishan.calculate;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class SimpleCalculator {
 
@@ -15,7 +17,19 @@ public class SimpleCalculator {
             remain = remain.substring(4);
         }
         String s[] = remain.split("["+delimiter+"\n]");
-        int sum = Arrays.stream(s).mapToInt(Integer::parseInt).sum();
+        List<Integer> nums = Arrays.stream(s).map(s2 -> {
+            try {
+                return Integer.parseInt(s2);
+            } catch (Exception e) {
+                throw new RuntimeException("Not valid String");
+            }
+        }).collect(Collectors.toList());
+        List<Integer> negativeNums = nums.stream().filter(n1 -> n1 < 0).collect(Collectors.toList());
+        if(!negativeNums.isEmpty()){
+            String exceptionMsg = negativeNums.stream().map(String::valueOf).collect(Collectors.joining(","));
+            throw new RuntimeException("negatives not allowed "+ exceptionMsg);
+        }
+        int sum = nums.stream().reduce(0, (a,b) -> a+b);
         return sum;
     }
 }
